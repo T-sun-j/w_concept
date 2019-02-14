fab8bce5b5c6729d95a6c19561ffc4a43edb1e1e
t-sun-j <15321580779@163.com>
2019-02-14 22:15:00 +0800

    2-14-6

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const proxy = require("http-proxy-middleware");
 
 module.exports=(app)=>{
     app.use("/api",proxy({
-        target:"http://m.maoyan.com",
+        target:"https://m.wconcept.cn",
         changeOrigin:true,
         pathRewrite:{
             "^/api":""
